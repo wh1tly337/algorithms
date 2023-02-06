c9cc82bef4daef4c4e79636b33fd4cee1b404bb9
wh1tly337 <wh1tly337@gmail.com>
2023-02-06 20:23:11 +0500

    fourth alg upd

--- CW1/fourth/fourth.py
+++ CW1/fourth/fourth.py
@@ -1,26 +1,34 @@
 def main():
-    # with open("input1.txt") as f:
-    #     input_values = f.readline().split(' ')  # сложность O(n) поскольку split делает посимвольную обработку
-    #
-    # result = int(input_values[0]) / int(input_values[1])
+    with open("input2.txt") as f:
+        input_values = f.readline().split(' ')  # сложность O(n) поскольку split делает посимвольную обработку
 
-    result = 500002 / 500001  # нужно сделать чтобы нули в начале не учитывались
-    print(result)
+    result = int(input_values[0]) / int(input_values[1])
 
-    result = str(result).split('.')
-    integer = result[0]
-    fractional = result[1]
+    # result = 500002 / 500001
+    integer, fractional = str(result).split('.')
     memory = []
-    for i in range(len(fractional) - 1):
-        for j in range(i, len(fractional)):
-            if fractional[i] == fractional[j] and fractional[i] not in memory:
-                memory.append(fractional[i])
-                break
+    repeat = ""
+    index = 0
+    max_zeros, ind_zeros = 0, 0  # считает нули в начале
+    while index < len(fractional):
+        if (fractional[index] in memory) and (fractional[index + 1] in memory):
+            repeat = fractional[:index]
+            if (len(repeat) - max_zeros) >= (len(fractional) // 2):
+                repeat = fractional
+            break
 
-    if len(fractional) / len(memory) >= 2:
-        print(f"{integer}.(" + "".join(memory) + ")")
+        if fractional[index] != '0':
+            memory.append(fractional[index])
+        else:
+            if index == ind_zeros:
+                max_zeros += 1
+                ind_zeros += 1
+        index += 1
+
+    if repeat:
+        print(f"{integer}.({repeat})")
     else:
-        print(f"{integer}.({fractional})")
+        print(f"{integer}.{fractional}")
 
 
 if __name__ == '__main__':
