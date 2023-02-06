6f501f34f219334bef5f9612dff90f1d420ea344
wh1tly337 <wh1tly337@gmail.com>
2023-02-06 18:19:31 +0500

    second alg upd

--- CW1/second/second.py
+++ CW1/second/second.py
@@ -1,48 +1,37 @@
 def main():
-    with open('input2.txt') as f:
+    with open('input1.txt') as f:
         input_dock = f.readlines()
     count_of_stones = int(input_dock[0])
-    all_stones = input_dock[1].split(' ')
+    all_stones = list(map(int, input_dock[1].split()))
+    close_result = sum(all_stones) // 2
 
-    sum_of_stones = 0
+    k = max(all_stones) + 1  # noqa
+    count = [0] * k
+    for i in all_stones:
+        count[i] += 1
+    j = 0
+    for i in range(k):
+        for _ in range(count[i]):
+            all_stones[j] = i
+            j += 1
+
+    prefix_sum = [0] * (count_of_stones + 1)
+    for i in range(1, count_of_stones + 1):
+        prefix_sum[i] = prefix_sum[i - 1] + int(all_stones[i - 1])
+
+    result = float('inf')
     for i in range(count_of_stones):
-        sum_of_stones += int(all_stones[i])
-    close_result = sum_of_stones // 2
+        if 2 >= close_result - prefix_sum[i] >= -2:
+            target_sum = prefix_sum[count_of_stones] - prefix_sum[i]
+            if prefix_sum[i] - target_sum < 0:
+                temporary = target_sum - prefix_sum[i]
+            else:
+                temporary = prefix_sum[i] - target_sum
+            if temporary < result and prefix_sum[i] - target_sum != 0:
+                result = temporary
 
-    changed = True  # noqa
-    while changed:
-        changed = False
-        for i in range(count_of_stones - 1):
-            if all_stones[i] < all_stones[i + 1]:
-                all_stones[i], all_stones[i + 1] = all_stones[i + 1], all_stones[i]
-                changed = True
-
-    try_close_result = 0
-    sum_of_stones = 0
-    result = 1000000000
-    for i in range(count_of_stones):
-        sum_of_stones += int(all_stones[i])
-        if 2 >= close_result - sum_of_stones >= -2:
-            for j in range(i + 1, count_of_stones):
-                try_close_result += int(all_stones[j])
-
-        if sum_of_stones - try_close_result < 0:
-            temporary = try_close_result - sum_of_stones
-        else:
-            temporary = sum_of_stones - try_close_result
-
-        if temporary < result and sum_of_stones - try_close_result != 0:
-            result = temporary
     print(result)
 
 
 if __name__ == '__main__':
     main()
-
-    # input 1:
-    # 0.03s
-    # 18.0 MiB
-
-    # input 2:
-    # 0.03s
-    # 18.1 MiB
