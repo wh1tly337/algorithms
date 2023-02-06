3f40f5ee97fee74eaf1c71b0cac92e05e328de36
wh1tly337 <wh1tly337@gmail.com>
2023-02-06 18:34:21 +0500

    third alg upd

--- CW1/third/third.py
+++ CW1/third/third.py
@@ -4,18 +4,12 @@ def main():
         count_of_values = int(input_dock[0])
         all_values = input_dock[1::]
 
-    result = 0
+    result, helper = 0, 0
     for i in range(count_of_values):
-        cycle_result = 0
-        helper = 0
-        for j in range(i, count_of_values):
-            if helper + int(all_values[j]) > cycle_result:
-                helper += int(all_values[j])
-                cycle_result = helper
-            else:
-                break
-        if cycle_result > result:
-            result = cycle_result
+        helper += int(all_values[i])
+        if helper < 0:
+            helper = 0
+        result = max(result, helper)
 
     print('Максимальная_сумма_подмассива =', result)
 
