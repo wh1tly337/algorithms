05953e2ebde3f9957cc26222f92898c4e1846b42
wh1tly337 <wh1tly337@gmail.com>
2023-03-06 16:43:36 +0500

    CW2 third solution added

--- CW2/third/third.py
+++ CW2/third/third.py
@@ -2,24 +2,22 @@ import operator
 
 
 def main():
-    input_value = []
+    line = []
     with open('input.txt') as f:
-        count = int(f.readline())
-        for i in range(count):
-            input_value.append(int(f.readline().replace('\n', '')))
-    sorted_mass = merge_sort(input_value)
-    three_plus, three_minus = 1, 1
-    for i in range(1, 4):
-        three_plus = three_plus * sorted_mass[-i]
-    for i in range(3):
-        if i < 2:
-            three_minus = three_minus * sorted_mass[i]
+        count_lines = int(f.readline())
+        for i in range(count_lines):
+            element = f.readline().replace('\n', '')
+            line.append(element)
+    mass = []
+    for i in range(count_lines):
+        action = line[i][0]
+        if action == '+':
+            mass.append(int(line[i].split(' ')[1]))
+        elif action == '-':
+            mass.remove(mass[0])
         else:
-            three_minus = three_minus * sorted_mass[-1]
-    if three_plus > three_minus:
-        print(three_plus)
-    else:
-        print(three_minus)
+            result = merge_sort(mass)
+            print(result[0])
 
 
 # noinspection DuplicatedCode
