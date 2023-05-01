cc136a71905911844746bc4a2193c18a8d9c1dd1
wh1tly337 <wh1tly337@gmail.com>
2023-05-01 12:01:43 +0500

    first solution fixed

--- CW3/first/first.py
+++ CW3/first/first.py
@@ -1,14 +1,14 @@
 def main():
     with open('input.txt') as f:
         n, m, k = map(int, f.readline().split())
-        standard = [set(map(int, f.readline().split())) for _ in range(n)]
-        similar = [set(map(int, f.readline().split())) for _ in range(k)]
+        standard = [list(map(int, f.readline().split())) for _ in range(n)]
+        similar = [list(map(int, f.readline().split())) for _ in range(k)]
+
+    [standard[i].sort() for i in range(n)]
+    [similar[i].sort() for i in range(k)]
 
     for i in similar:
-        if i in standard:
-            print(1)
-        else:
-            print(0)
+        print(1) if i in standard else print(0)
 
 
 if __name__ == '__main__':
